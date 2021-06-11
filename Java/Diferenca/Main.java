1775c913119ddedc73d3aef0ac6dec6cac637e0b
unknown <flavioportugal20@gmail.com>
2021-06-11 11:18:41 -0300

    Correções

--- Java/Diferenca/Main.java
+++ Java/Diferenca/Main.java
@@ -4,39 +4,22 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	  
+
 	static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 	public static void main(String[] args) throws IOException {
-		int par = 0;
-		int impar = 0; 
-		int positvo = 0; 
-		int negativo = 0;
-		  
-		for(int i=0; i<5; i++) {
-			int x = Integer.parseInt(ler());
-			if(x%2 == 0){
-				par++;
-			}else{
-				impar++;
-			}		    	
-			if(x>0){
-				positvo++;
-			}else if(x<0){
-				negativo++;
-			}		    	  	
-		}
+		int a = Integer.parseInt(ler());
+		int b = Integer.parseInt(ler());
+		int c = Integer.parseInt(ler());
+		int d = Integer.parseInt(ler());
+		int diferenca = a * b - c * d;
 
-		System.out.println(par + " valor(es) par(es)");
-		System.out.println(impar + " valor(es) impar(es)");
-		System.out.println(positvo + " valor(es) positivo(s)");
-		System.out.println(negativo+ " valor(es) negativo(s)");
-		  
-		br.close();      
+		System.out.printf("DIFERENCA = %s", diferenca);
+		br.close();
 	}
-	
+
 	static String ler() throws IOException {
 		return new StringTokenizer(br.readLine()).nextToken();
 	}
-	
+
 }
