2c871a0042917e5fb3c963c23f378109c33716bb
unknown <flavioportugal20@gmail.com>
2021-06-29 20:10:15 -0300

    alteracao no desafio media 3

--- Java/Média 3/Main.java
+++ Java/Média 3/Main.java
@@ -5,65 +5,65 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-  public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException {
 
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	  
-    double mediaPonderada, exame, mediaFinal;
-    float[] pesos = {2.0f, 3.0f, 4.0f, 1.0f};
-	float[] notas = new float[pesos.length];
-    StringTokenizer stNotas = lerST(br);
-    
-    int i = 0;
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		double mediaPonderada, exame, mediaFinal;
+		float[] pesos = { 2.0f, 3.0f, 4.0f, 1.0f };
+		float[] notas = new float[pesos.length];
+		StringTokenizer stNotas = lerST(br);
+
+		int i = 0;
 		while (stNotas.hasMoreTokens()) {
-		  if (i >= notas.length) {
+			if (i >= notas.length) {
 				System.out.println("Quantidade de notas diferente da quantidade de pesos!");
 				return;
 			}
 			notas[i] = Float.valueOf(stNotas.nextToken());
 			i++;
 		}
-    
-    mediaPonderada = calcularMediaPonderada(notas, pesos);
-    System.out.printf("Media: %.1f\n" , mediaPonderada);
 
-    if (mediaPonderada >= 7.0) {
-      System.out.printf("Aluno aprovado.\n");
-    } else if (mediaPonderada < 5.0) {
-      System.out.println("Aluno reprovado.\n");
-    }else if (mediaPonderada >= 5.0 && mediaPonderada <= 6.9) {
+		mediaPonderada = calcularMediaPonderada(notas, pesos);
+		System.out.printf("Media: %.1f\n", mediaPonderada);
 
-      System.out.printf("Aluno em exame.\n");
-      exame = Double.valueOf(ler(br));
-      System.out.printf("Nota do exame: %.1f\n", exame);
+		if (mediaPonderada >= 7.0) {
+			System.out.printf("Aluno aprovado.\n");
+		} else if (mediaPonderada < 5.0) {
+			System.out.println("Aluno reprovado.\n");
+		} else if (mediaPonderada >= 5.0 && mediaPonderada <= 6.9) {
 
-      mediaFinal = (mediaPonderada + exame) / 2;
+			System.out.printf("Aluno em exame.\n");
+			exame = Double.valueOf(ler(br));
+			System.out.printf("Nota do exame: %.1f\n", exame);
 
-      if (mediaFinal >= 5.0) {
-        System.out.printf("Aluno aprovado.\n");
-      } else if (mediaFinal < 5.0) {
-        System.out.printf("Aluno reprovado\n");
-      }      
-      System.out.printf("Media final: %.1f\n", mediaFinal);
-    }
+			mediaFinal = (mediaPonderada + exame) / 2;
 
-    br.close();
-  }
-  
-  static float calcularMediaPonderada(float[] notas, float[] pesos) {
+			if (mediaFinal >= 5.0) {
+				System.out.printf("Aluno aprovado.\n");
+			} else {
+				System.out.printf("Aluno reprovado\n");
+			}
+			System.out.printf("Media final: %.1f\n", mediaFinal);
+		}
+
+		br.close();
+	}
+
+	static float calcularMediaPonderada(float[] notas, float[] pesos) {
 		float somaNotasXPesos = 0.0f;
 		float totalPesos = 0.0f;
-		for (int i=0; i<pesos.length; i++) {
+		for (int i = 0; i < pesos.length; i++) {
 			somaNotasXPesos += notas[i] * pesos[i];
 			totalPesos += pesos[i];
 		}
 		return somaNotasXPesos / totalPesos;
 	}
-	
+
 	static String ler(BufferedReader br) throws IOException {
 		return new StringTokenizer(br.readLine()).nextToken();
 	}
-	
+
 	static StringTokenizer lerST(BufferedReader br) throws IOException {
 		return new StringTokenizer(br.readLine());
 	}
